77e6b59352c3f9351a3f2fc90e2574eda9808bba
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-05-14 19:08:24 +0800

    feat: attempt to fallback to local vivid if built theme is unavailable

--- vivid-zsh.plugin.zsh
+++ vivid-zsh.plugin.zsh
@@ -1,8 +1,20 @@
+set -euo pipefail
+
+ANSI_RED="\e[0;31m"
+ANSI_RESET="\e[0m"
+
 themes_dir="${0:a:h}/themes"
-
-[ ! $vivid_theme ] && echo "vivid_theme is not set!" && return 1
-
 theme_file="$themes_dir/$vivid_theme.zsh"
-[ ! -f $theme_file ] && echo "theme $vivid_theme could not be found!" && return 1
+
+[ ! "$vivid_theme" ] && echo "${ANSI_RED}vivid_theme is not set!${ANSI_RESET}" && return 1
+
+if [ ! -f "$theme_file" ]; then
+  if ! export LS_COLORS="$(vivid generate $vivid_theme)"; then
+    echo "${ANSI_RED}theme $vivid_theme does not exist!${ANSI_RESET}" 
+    return 1
+  else
+    return 0
+  fi
+fi
 
 source "$theme_file"
