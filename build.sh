abc9cb20313c07c598b6b7c67c87491317efa5e1
seth <getchoo@tuta.io>
2023-05-14 04:19:24 -0400

    ci: adopt Nix and improve build.sh (#1)
    * feat: make ci faster
    * fix: use `/usr/bin/env bash` in build.sh
    * fix: catch errors in build.sh
    * fix(ci): use pure nix shell

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 mkdir -p "plugins"
 
 for theme in $(vivid themes); do
-    echo "export LS_COLORS=\"$(vivid generate $theme)\"" > "plugins/vivid-$theme.zsh"
+    echo "export LS_COLORS=\"$(vivid generate "$theme")\"" > "plugins/vivid-$theme.zsh"
 done
