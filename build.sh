a76616c87821e96ae0cbdfb6bf897bffc170a283
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-07 10:37:15 +0800

    feat: improve build script

--- build.sh
+++ build.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
+
+ansi_dim="\033[2m"
+ansi_reset="\033[0m"
+ansi_green="\033[32m"
+
+if [ -n "$NO_COLOR" ]; then
+	ansi_dim=""
+	ansi_reset=""
+	ansi_green=""
+fi
 
 generate_dir="themes"
 
 rm -rf "$generate_dir"
 mkdir -p "$generate_dir"
 
+vivid --version
 for theme in $(vivid themes); do
-    echo "export LS_COLORS=\"$(vivid generate "$theme")\"" > "$generate_dir/$theme.zsh"
+	echo "export LS_COLORS=\"$(vivid generate "$theme")\"" > "$generate_dir/$theme.zsh" || exit 1
+	echo -e "${ansi_dim}built $generate_dir/${ansi_reset}${ansi_green}$theme.zsh${ansi_reset}"
 done
--- vivid-zsh.plugin.zsh
+++ vivid-zsh.plugin.zsh
@@ -7,12 +7,12 @@ theme_file="$themes_dir/$vivid_theme.zsh"
 [ ! "$vivid_theme" ] && echo "${ANSI_RED}vivid_theme is not set!${ANSI_RESET}" && return 1
 
 if [ ! -f "$theme_file" ]; then
-  if ! export LS_COLORS="$(vivid generate $vivid_theme)"; then
-    echo "${ANSI_RED}theme $vivid_theme does not exist!${ANSI_RESET}" 
-    return 1
-  else
-    return 0
-  fi
+	if ! export LS_COLORS="$(vivid generate $vivid_theme)"; then
+		echo "${ANSI_RED}theme $vivid_theme does not exist!${ANSI_RESET}" 
+		return 1
+	else
+		return 0
+	fi
 fi
 
 source "$theme_file"
